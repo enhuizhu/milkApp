97920804a91ed9a3c68fab9a7a20c0532fbd2e8a
david2013 <david@rocabee.com>
2014-05-26 14:42:08 +0100

    add function to controller

--- index.php
+++ index.php
@@ -5,11 +5,19 @@
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
 <script language="javascript" src="js/appController.js"></script>
 </head>
-<body>
+<body ng-controller="milkCtrl">
    <div>
-       <label>name:<label>
-       <input type="text" ng-model="username">
-       <h1>hello,{{username}}</h1>
+      <form>
+	     <label>milk name:</label>
+		 <input type="text" ng-model="milkName"><br>
+		 <label>milk price</label>
+	     <input type="text" ng-model="milkPrice"><br>
+	     <button ng-click="addNewMilk(milkName,milkPrice)">Add New Milke</button>
+	  
+      <ul ng-repeat="milk in milks">
+	     <li>{{milk.name+":&pound;"+milk.price}} <button ng-click="deleteMilk($index)">delete</button></li>
+	  </ul>
+	  </form>
    </div> 
 </body>
 </html>
--- js/appController.js
+++ js/appController.js
@@ -1,12 +1,31 @@
 /**
 * angular controller is here!
 **/
-var onlineMilk= augular.module("onlineMilk",[]);
+var onlineMilk= angular.module("onlineMilk",[]);
 onlineMilk.controller("milkCtrl",function($scope){
    $scope.milks=[
       {"name":"cowGate","price":"7.99"},
       {"name":"aiTamei","price":"10.00"},
       {"name":"huiShi","price":"10.00"}
    ];
+   
+   /**
+   * add new milk to the list
+   **/
+   $scope.addNewMilk=function(milkName,milkPrice){
+      var milkObj={}
+      milkObj.name=milkName;
+      milkObj.price=milkPrice;
+      $scope.milks.push(milkObj);	  
+   }
+   
+   /**
+   * delete miik from the list
+   **/
+   $scope.deleteMilk=function(arrayIndex){
+      console.log("index is:",arrayIndex);
+	  $scope.milks.splice(arrayIndex,1);
+   }
+   
 }); 
 
